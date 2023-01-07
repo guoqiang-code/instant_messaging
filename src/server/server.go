72dc1636810f028130ab927e20c6586f163774b8
郭强 <294913106@qq.com>
2023-01-07 21:36:24 +0800

    修复

--- src/server/server.go
+++ src/server/server.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	curUser "instant_messaging/src/online_user"
 	"net"
+	"strings"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -105,7 +107,10 @@ func (s *Server) Offline(u *curUser.User) {
 
 // SendMsg 用户发送消息
 func (s *Server) SendMsg(u *curUser.User) {
+	isLive := make(chan bool)
 	for {
+		go s.timeoutExit(isLive, u)
+
 		bytes := make([]byte, 4096)
 		read, err := u.Conn.Read(bytes)
 		if err != nil {
@@ -116,15 +121,16 @@ func (s *Server) SendMsg(u *curUser.User) {
 			return
 		}
 		msg := string(bytes[:read])
+		isLive <- true
 		if msg == "search" {
 			fmt.Println("用户查询在线用户列表……………………")
 			s.MapLock.Lock()
+			sendClientMsg := ""
 			for _, user := range s.OnlineMap {
-				sendClientMsg := "用户为：title【" + user.Title + "】,addr为：【" + user.Addr + "】的用户在线"
-				user.SendMsgClient(sendClientMsg)
+				sendClientMsg += "\n用户为：title【" + user.Title + "】,addr为：【" + user.Addr + "】的用户在线"
 			}
+			u.SendMsgClient(sendClientMsg)
 			s.MapLock.Unlock()
-			return
 		} else if len(msg) > 7 && msg[:6] == "rename" {
 			s.MapLock.Lock()
 			delete(s.OnlineMap, u.Title)
@@ -132,9 +138,39 @@ func (s *Server) SendMsg(u *curUser.User) {
 			s.OnlineMap[u.Title] = u
 			s.MapLock.Unlock()
 			u.SendMsgClient("用户名称修改成功……")
+		} else if len(msg) > 3 && msg[:2] == "to" {
+			split := strings.Split(msg, " ")
+			if len(split) != 3 {
+				fmt.Println("消息发送格式不匹配…………")
+			}
+			user, ok := s.OnlineMap[split[1]]
+			if !ok {
+				fmt.Println("指定用户：" + split[1] + " 不存在")
+				u.SendMsgClient("指定用户：" + split[1] + "不存在")
+			} else {
+				user.SendMsgClient("用户：" + u.Title + "给您发送消息-->" + split[2] + "\n")
+			}
 		} else {
 			s.BoardCast(u, msg)
 		}
 
 	}
+}
+
+func (s *Server) timeoutExit(isLive chan bool, user *curUser.User) bool {
+	select {
+	case <-isLive:
+	//不做处理，表示有消息
+	case <-time.After(time.Second * 120):
+		//表示超过十秒未发送消息，超时踢出
+		fmt.Println("用户 " + user.Title + " 超时下线")
+		user.SendMsgClient("超时下线")
+		err := user.Conn.Close()
+		if err != nil {
+			return true
+		}
+		close(user.Channel)
+		return true
+	}
+	return false
 }
